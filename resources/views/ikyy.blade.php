392080373113bc896d6d518998fbe7f7eb7ff06c
ikyykun <152145771+ikyykun@users.noreply.github.com>
2024-02-19 09:50:30 +0700

    ikyy page

---
+++ resources/views/ikyy.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ikyy page</title>
+</head>
+<body>
+    <p>Ikyy membuat perubahan</p>
+    <h5>Mixer andalan bangsa membawa amanah jokyoh</h5>
+    <h4>Presiden Gachikoi 2024</h4>
+</body>
+</html>
